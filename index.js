450c0e11f78cd813b78be7ba6f31aac7ade11efb
jamelanogaster-smkrus <jamil@smkrus.sch.id>
2022-06-03 09:00:57 +0700

    menambahkan jawaban

--- index.js
+++ index.js
@@ -8,7 +8,19 @@ const dates = [
 ];
 
 // TODO: Buatlah fungsi createDate
-const createDate = null;
+const createDate = (dataBaru, index) => {
+  const sortDate = [];
+    for (let i = 0 ; i < dataBaru.length ; i++){
+      const result = (new Date(dataBaru[i]).getTime() / 1000).toString(); sortDate.push(result);
+    };
+
+    if (index === undefined){
+      sortDate.sort((a, b) => a - b);
+        return sortDate.join("-");
+     } else {
+        return sortDate[index];
+     }
+};
 
 // ! JANGAN DIMODIFIKASI
 (() => {
